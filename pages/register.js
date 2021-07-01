ddd646d16e248c50773e091b5749da3c4d611c44
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-01 23:32:42 +0300

    register - request added

--- pages/register.js
+++ pages/register.js
@@ -1,20 +1,44 @@
 export default function register(){
-    const [username, usernameInput] = useInput({type: 'text', placeholder: 'username', validationRules: {
-        minLength: 6,
-        maxLength: 15,
-        required: true
-    }}) 
-    const [password, passwordInput] = useInput({type: 'password', placeholder: 'password', validationRules:{
-        minLength: 7,
-        maxLength: 25,
-        required: true
-    }}) 
-    const [repeat, repeatInput] = useInput({type: 'password', placeholder: 'repeat', validationRules:{
-        required: true,
-    },equalsValue: password, equalsName: 'Passwords'}) 
-    const [firstName, firstNameInput] = useInput({type: 'text', placeholder: 'first name', validationRules: {
-        required: true
-    }}) 
+    const [registeredUser, fetchRegister, error] = useRequest({
+        initialUrl: `${localStorage.getItem('BaseUrl')}/api/users/${localStorage.getItem('LongPolling')}/register`, 
+        shouldThrow: false, 
+        callback: 
+        setAuthUser, 
+        type: 'post'
+    })
+    
+    const [username, usernameInput] = useInput({
+        type: 'text', 
+        placeholder: 'username', 
+        validationRules: {
+            minLength: 6,
+            maxLength: 15,
+            required: true
+        }
+    }) 
+    const [password, passwordInput] = useInput({
+        type: 'password', 
+        placeholder: 'password', 
+        validationRules:{
+            minLength: 7,
+            maxLength: 25,
+            required: true
+        }
+    }) 
+    const [repeat, repeatInput] = useInput({
+        type: 'password', 
+        placeholder: 'repeat', 
+        validationRules: {
+            required: true,
+        },
+        equalsValue: password, 
+        equalsName: 'Passwords'
+    }) 
+
+    const register = (e) => {
+        e.preventDefault()
+        fetchRegister({body:{username, password, repeat, firstName, lastName, country, age}})
+    }
     
     return(
         <form onSubmit={(e) => setPage(e, 1)}>
