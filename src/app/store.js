651c4f060db8508bcf355a1d67c9984cff3099f7
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-10 23:51:34 +0300

    store added

---
+++ src/app/store.js
@@ -0,0 +1,31 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import authenticate from './slices/authenticate';
+import createSaga from 'redux-saga';
+import IndexSagas from './sagas';
+
+const sagaMiddleware = createSaga();
+const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
+
+const combinedReducer = combineReducers({
+    authenticate,
+})
+
+const rootReducer = (state, action) => {
+    if (action.type === 'authenticate/onLogout') { 
+        localStorage.removeItem('Authorization');
+        localStorage.removeItem('user');
+
+        return combinedReducer(undefined, action);
+    }
+
+    return combinedReducer(state, action);
+}
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware
+});
+
+sagaMiddleware.run(IndexSagas);
+
+export default store
