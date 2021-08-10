4235aaa34a76905be36e07ce84761a0c5c0f3012
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-10 23:56:04 +0300

    notes watcher added

---
+++ src/app/sagas/notes.js
@@ -0,0 +1,56 @@
+import { takeLatest, select, put } from 'redux-saga/effects';
+import { BASE_URL } from '../../constants';
+import { getNotesData, onNotesComplete, onNotesError} from '../slices/notesSlice';
+
+export default takeLatest('notes/notesRequest', getNotes);
+
+function* getNotes({payload: query}){
+    const { name, lastName, lastId, takeAmount } =  getData(query, yield select(getNotesData));
+    const lastPath = lastName ? `${lastName}/${lastId}` : '';
+
+    const response = yield fetch(`${BASE_URL}/api/notes/auth/searchForNotes/${name}/${takeAmount}/${lastPath}`,{
+        headers:{
+            Authorization: `Bearer ${localStorage.getItem('Authorization')}`
+        }
+    });
+
+    if(response.ok){
+        const data = yield response.json(); 
+        
+        data.length = data.notes.length;
+        data.lastNote = data.notes[data.length];
+        data.notes = splitArray(data.notes, take);
+
+        yield put(onNotesComplete({
+            data,
+            query
+        }))
+    }else{
+        yield put (onNotesError(response.text()));
+    }
+
+}
+
+const splitArray = (notes, take) => {
+    return notes.reduce((result, note, i) =>  {
+        const page = Math.floor(i / take);
+        result[page] = result[page] ? (result[page].push(note), result[page]) : [note];
+
+        return result;
+    },[])
+}
+
+const getData = (query, data) => {
+    let lastId = 0;
+    let lastName;
+    let lastNote = data.lastNote;
+    const takeAmount = query.take * query.pages;
+    
+    if(lastNote){
+        lastId = lastNote.id;
+        lastName = lastNote.name;
+    }
+
+    return {...query, takeAmount, lastName, lastId}
+}
+
