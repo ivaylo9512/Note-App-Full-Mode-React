e57bd4bf438415271af2022d2ee43c748f35cdeb
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-16 23:53:06 +0300

    unit tests Login

---
+++ src/components/Login/test/unit.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { loginRequest, getLoginRequest, getRegisterRequest } from '../../../app/slices/authenticateSlice';
+import { shallow } from 'enzyme';
+import Login from '../Login';
+
+jest.mock('react-redux', () => ({
+    useSelector: jest.fn().mockImplementation(fn => fn()),
+    useDispatch: () => jest.fn()
+}))
+
+jest.mock('../../../app/slices/authenticateSlice')
+
+const createWrapper = (state) => {
+    getRegisterRequest.mockReturnValue(state);
+
+    return shallow(
+        <Login />
+    )
+}
+
+describe('Login unit tests', () => {
+    it('should render inputs', () => {
+        const wrapper = createWrapper({ isLoading: false, error: null });
+
+        expect(wrapper.find('input').length).toBe(2);
+        expect(wrapper.findByTestid('username').length).toBe(1);
+        expect(wrapper.findByTestid('password').length).toBe(1);
+    })
+
+    it('should render error', () => {
+        const wrapper = createWrapper({ isLoading: false, error: 'Bad credentials'});
+
+        expect(wrapper.findByTestid('error').text()).toBe('Bad credentials.')
+    })
+
+    it('should change inputs values', () => {
+        const wrapper = createWrapper({isLoading: false, error: null});
+
+        wrapper.findByTestid('username').simulate('change', { target: { value: 'username' }});
+        wrapper.findByTestid('password').simulate('change', { target: { value: 'password' }});
+
+        expect(wrapper.findByTestid('username').prop('value')).toBe('username');
+        expect(wrapper.findByTestid('password').prop('value')).toBe('password');
+    })
+})
